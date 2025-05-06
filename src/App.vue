698de1436ce7063d6c964775ed0d17bca879ded3
Shumaila <shumaila.1592@gmail.com>
2025-05-07 00:22:42 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script >
+import Todo from './components/Todo.vue'
+export default {
+  name:'App',
+  components:{
+    Todo
+  }
+}
+</script>
+
+<template>
+ 
+<Todo></Todo>
+ 
+</template>
+
+<style scoped>
+
+
+
+</style>
